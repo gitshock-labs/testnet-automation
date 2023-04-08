6c9d905474bfe738811930c7c8aaf121dd06b70f
Avenbreaks <dev-cosmware@gitshock.com>
2023-04-09 05:26:01 +0800

    Update create-deposit.sh
    remove ethereal old, adding new ethereal

--- create-deposit.sh
+++ create-deposit.sh
@@ -1,6 +1,28 @@
-#!/bin/bash
-set-x
+amount=32000000000
+smin=0
+smax=1
 
-function_deposit() {
-    ./clients/deposit.sh new-mnemonic --num_validators 1 --chain cartenz --eth1_withdrawal_address 0x9adddA86C9479C45bD145BBa9FC28146FdF46C83 --folder=./validator_keys
-}
+eth2-val-tools deposit-data \
+  --amount=$amount \
+  --fork-version=0x00677693 \
+  --source-min=$smin \
+  --source-max=$smax \
+  --withdrawals-mnemonic="YOUR-MNEMONIC" \
+  --validators-mnemonic="YOUR-MNEMONIC" > cartenz_deposits_$smin\_$smax.txt
+
+while read x; do
+   account_name="$(echo "$x" | jq '.account')"
+   pubkey="$(echo "$x" | jq '.pubkey')"
+   echo "Sending deposit for validator $account_name $pubkey"
+   ethereal beacon deposit \
+      --allow-unknown-contract=true \
+      --address="0x4242424242424242424242424242424242424242" \
+      --connection=https://rpc-phase1.cartenz.works \
+      --data="$x" \
+      --allow-excessive-deposit \
+      --value="$amount" \
+      --from="0x9adddA86C9479C45bD145BBa9FC28146FdF46C83" \
+      --privatekey="YOUR-PRIVATEKEY-HERE"
+   echo "Sent deposit for validator $account_name $pubkey"
+   sleep 2
+done < cartenz_deposits_$smin\_$smax.txt
